29b0e88c7da15c8c62e78d4b087ab1de1b745840
premprakashIT <prem.mca16@mjpru.ac.in>
2024-04-19 16:22:42 +0530

    Added some functionality using js

---
+++ Oneday/public/script.js
@@ -0,0 +1,9 @@
+// function PrintPage(){
+//     window.print();
+// }
+
+function bodyload(){
+    window.document.images[0].src="eye.jpg";
+    window.document.forms[0].elements[1].value="Register";
+
+}
